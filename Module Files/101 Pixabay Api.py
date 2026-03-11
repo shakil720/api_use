3bc48b1b3bb73a6f79513019277649607e051d2d
shakil720 <mahmudshakil720@gmail.com>
2026-03-11 06:24:12 +0600

    New added

---
+++ Module Files/101 Pixabay Api.py
@@ -0,0 +1,15 @@
+from httpx import get
+from pprint import pprint
+
+api_key = "54975452-35364a4966174202e8083335b"
+
+url = f"https://pixabay.com/api/?key={api_key}&q=yellow+flowers&image_type=photo"
+
+
+r = get(url)
+api_data = r.json()
+image_url = api_data.get("hits")
+hits = image_url[0].get("pageURL")
+
+
+print(hits)
